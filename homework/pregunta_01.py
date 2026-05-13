a80a9643e9efd8cc04d7615e17f595fad423634b
mbarrios234 <mbarriosm@unal.edu.co>
2026-05-12 20:33:39 -0500

    Lab3

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,6 +3,8 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
 # pylint: disable=import-outside-toplevel
+import re
+import pandas as pd
 
 
 def pregunta_01():
@@ -18,3 +20,41 @@ def pregunta_01():
 
 
     """
+
+    filas = []
+    with open('files/input/clusters_report.txt') as archivo:
+        filas = archivo.readlines()
+    filas = filas[4:]
+    clusters = []
+    base = {
+        "numero": 0,
+        "cantidad": 0,
+        "porcentaje": 0,
+        "palabras": ""
+    }
+
+    for fila in filas:
+        if re.match('^ +[0-9]+ +', fila):
+            numero, cantidad, porcentaje, *palabras = fila.split()
+            base["porcentaje"] = float(porcentaje.replace(',','.'))
+            base["cantidad"] = int(cantidad)
+            base["numero"] = int(numero)
+            palabras = ' '.join(palabras[1:])
+            base["palabras"] += palabras
+        elif re.match('^\n', fila) or re.match('^ +$', fila):
+            base["palabras"] = base["palabras"].replace('.', '')
+            clusters.append(base.values())
+            base = {
+                "numero": 0,
+                "cantidad": 0,
+                "porcentaje": 0,
+                "palabras": ""
+            }
+            
+        elif re.match('^ +[a-z]', fila):
+            palabras = fila.split()
+            palabras = ' '.join(palabras)
+            base["palabras"] += ' ' + palabras
+    columnas = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave']
+    df = pd.DataFrame (clusters, columns = columnas )
+    return df
